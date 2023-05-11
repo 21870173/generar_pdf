d58c0ac5d7b498c77b0381754d5ac5c1f614dfdc
21870173 <mfriboldi@hotmail.com>
2023-05-11 10:53:20 -0300

    generar pdf

---
+++ script.js
@@ -0,0 +1,11 @@
+function pdf() {
+    // Declarar variable
+    let doc = new jsPDF();
+
+    // Escribir texto
+    doc.text(20, 20, "Hola Chicos!");
+    doc.text(20, 30, "Recuerden que mañana es el último día de inscripción Tramo 2" + "\n\nÉxitos!!");
+
+    // Generar y guardar PDF
+    doc.save('hola_chicos.pdf');
+};
